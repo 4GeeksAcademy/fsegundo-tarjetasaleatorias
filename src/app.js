5f98ab65189ab05ac821ebcb11b9107e83a78ae7
Segundo Ferreira <segundo.ferreira28@gmail.com>
2025-04-07 19:29:50 +0000

    ultimos cambios del lunes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+const sticks = ["♠", "♣", "♥", "♦"];
+const numbersRandom = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+const sticksRandom = sticks[Math.floor(Math.random() * sticks.length)];
+const numbers = numbersRandom[Math.floor(Math.random() * numbersRandom.length)];
+
+
+
+
+window.onload = function () {
   //write your code here
 
 };
